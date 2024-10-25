d217a565bdb600578022e08c497e3e7ca01416ff
Debarpan102 <debarpanhalder8@gmail.com>
2024-10-25 20:44:05 +0530

    DroidX + User info changes

--- aosp_script.sh
+++ aosp_script.sh
@@ -7,26 +7,26 @@
 # sudo apt install nano tmux htop neofetch btop
 
 MY_ROOT_DIR="~"
-MY_WORKSPACE_DIR="derp"
-MY_ROM="https://github.com/DerpFest-AOSP/manifest.git"
-MY_ROM_BRANCH="15"
-MY_LOCAL_MANIFEST="15-derp"
+MY_WORKSPACE_DIR="droidx"
+MY_ROM="https://github.com/DroidX-UI/manifest.git"
+MY_ROM_BRANCH="14_v3"
+MY_LOCAL_MANIFEST="14-droidx"
 CUSTOMCLANG="r487747c"
-MY_EMAIL="singhsamarjeet2007@gmail.com"
-MY_USERNAME="DevInfinix"
-DIRKEYS="vendor/derp/signing/keys/"
-KEYS_BRANCH="15.0-derp"
+MY_EMAIL="debarpanhalder8@gmail.com"
+MY_USERNAME="Debarpan102"
+DIRKEYS="vendor/aosp/signing/keys"
+KEYS_BRANCH=" "
 
-LUNCH_CMD=derp_ice-userdebug
-MAKE_CMD=derp
+LUNCH_CMD=droidx_ice-a2pa-userdebug
+MAKE_CMD=m bacon
 
 export USE_CCACHE=1
 export CCACHE_EXEC=/usr/bin/ccache
 export CCACHE_COMPRESS=1
 MY_CCACHE_SIZE=40G
 
-export BUILD_USERNAME=DevInfinix
-export BUILD_HOSTNAME=Garudinix
+export BUILD_USERNAME=Debarpan
+export BUILD_HOSTNAME=Linux
 
 
 ################################################################
@@ -58,9 +58,9 @@ echo "========================================================================"
 
 repo init -u $MY_ROM -b $MY_ROM_BRANCH --git-lfs
 
-git clone https://github.com/DevInfinix/android-aosp-local-manifests.git -b $MY_LOCAL_MANIFEST .repo/local_manifests
+git clone https://github.com/Debarpan102/android-aosp-local-manifests.git -b $MY_LOCAL_MANIFEST .repo/local_manifests
 
-repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
+repo sync -c -j$(nproc --all) --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune
 
 echo "========================================================================"
 echo "SYNCING FINISHED"
@@ -84,16 +84,16 @@ echo "========================================================================"
 ################################################################
 
 # Check if the directory exists
-if [ -d "$DIRKEYS" ]; then
-    echo "Directory $DIRKEYS exists. Deleting it..."
+#if [ -d "$DIRKEYS" ]; then
+#   echo "Directory $DIRKEYS exists. Deleting it..."
     rm -rf "$DIRKEYS"
-    echo "Directory deleted."
-else
-    echo "Directory $DIRKEYS does not exist. No need to delete."
-fi
+#   echo "Directory deleted."
+#else
+ #   echo "Directory $DIRKEYS does not exist. No need to delete."
+#fi
 
-echo "Cloning the repository..."
-git clone https://github.com/DevInfinix/devinfinix-aosp-roms-keys --depth=1 -b $KEYS_BRANCH "$DIRKEYS"
+#echo "Cloning the repository..."
+#git clone https://github.com/DevInfinix/devinfinix-aosp-roms-keys --depth=1 -b $KEYS_BRANCH "$DIRKEYS" **/
 
 echo "========================================================================"
 echo "CLONED KEYS"
